168b65381ff2950e1c51490cb2a67fadddb18eb7
John-MBDP <johnartuz@hotmail.com>
2022-05-27 00:37:04 -0400

    Implement random picker to select city and state from cities data

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
+const cities = require("./cities");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -14,8 +15,13 @@ db.once("open", () => {
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  const c = new Campground({ title: "purple field" });
-  await c.save();
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await camp.save();
+  }
 };
 
 seedDB();
