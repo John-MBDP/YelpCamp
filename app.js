96eb93cfbc7d81854945b746759f11cae78538d4
John-MBDP <johnartuz@hotmail.com>
2022-05-30 18:57:22 -0400

    Implement server side validation when a user creates a new campground

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const Joi = require("joi");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const morgan = require("morgan");
-const { nextTick } = require("process");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -49,7 +49,25 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+    // if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+
+    //Validate Data before attempting to save with mongoose
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+
+    const { error } = campgroundSchema.validate(req.body);
+
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
