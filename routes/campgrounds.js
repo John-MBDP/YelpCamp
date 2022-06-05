02374b5d2178b1fc4471cbed09dd30a492d54853
John-MBDP <johnartuz@hotmail.com>
2022-06-04 23:29:10 -0400

    Create routes folder

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,63 +1,5 @@
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const { campgroundSchema, reviewSchema } = require("./schemas");
-const ejsMate = require("ejs-mate");
-const catchAsync = require("./utils/catchAsync");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const morgan = require("morgan");
-const Review = require("./models/review");
-
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
-
-const app = express();
-
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(morgan("tiny"));
-
-const validateCampground = (req, res, next) => {
-  //Validate Data before attempting to save with mongoose
-
-  const { error } = campgroundSchema.validate(req.body);
-
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
+const router = express.router();
 app.get(
   "/campgrounds",
   catchAsync(async (req, res) => {
@@ -140,19 +82,3 @@ app.delete(
     res.redirect(`/campgrounds/${id}`);
   })
 );
-
-app.all("*", (req, res, next) => {
-  next(new ExpressError("Page Not Found", 404));
-});
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-
-  if (!err.message) err.message = "Oh No, Something Went Wrong!";
-
-  res.status(statusCode).render("error", { err });
-});
-
-app.listen(3000, () => {
-  console.log("Serving on port 3000");
-});
