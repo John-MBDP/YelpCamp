43f369d4c52dd1240eac3ef568b59747ca08e7bb
John-MBDP <johnartuz@hotmail.com>
2022-06-07 12:55:47 -0400

    Add Error flash message

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,7 +46,10 @@ router.get(
   "/:id",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate("reviews");
-    // console.log(campground);
+    if (!campground) {
+      req.flash("error", "Campground not found");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
   })
 );
