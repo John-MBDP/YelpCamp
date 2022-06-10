6cc01e5a20a553a6929658dc3bd467291b48b8ea
John-MBDP <johnartuz@hotmail.com>
2022-06-10 12:31:06 -0400

    Refactor routes to use router.route

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,15 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const users = require("../controllers/users");
 
-router.get("/register", users.renderRegister);
+router.route("/register").get(users.renderRegister).post(catchAsync(users.createUser));
 
-router.post("/register", catchAsync(users.createUser));
-
-router.get("/login", users.renderLogin);
-
-router.post(
-  "/login",
-  passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }),
-  users.login
-);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }),
+    users.login
+  );
 
 router.get("/logout", users.logout);
 
