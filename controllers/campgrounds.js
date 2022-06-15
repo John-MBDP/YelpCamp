4a7686b4d9d7644bb1c2dff7f07f545e7f07b929
John-MBDP <johnartuz@hotmail.com>
2022-06-14 23:47:51 -0400

    Fix bug with updateCampground logic duplication.  Add multipart/form-data to the edit form

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -42,12 +42,14 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findById(id);
-  if (!campground.author.equals(req.user._id)) {
-    req.flash("error", "You do not have permission to do that");
-    return res.redirect(`/campgrounds/${id}`);
-  }
-  const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  // if (!campground.author.equals(req.user._id)) {
+  //   req.flash("error", "You do not have permission to do that");
+  //   return res.redirect(`/campgrounds/${id}`);
+  // }
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  await campground.save();
   req.flash("success", "Successfully updated campground");
   res.redirect(`/campgrounds/${campground._id}`);
 };
