af7aa2cef6986e22fbe4a1f7e3ccfe9ade07c5c3
John-MBDP <johnartuz@hotmail.com>
2022-06-20 17:48:27 -0400

    Deploying for launch

--- app.js
+++ app.js
@@ -25,9 +25,10 @@ const userRoutes = require("./routes/users");
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-// const dbUrl = process.env.DB_URL
+const MongoDBStore = require("connect-mongo")(session);
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -48,9 +49,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
+
+const secret = process.env.SECRET  || "secret!"
+
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60
+})
+
+store.on("error", function(e) {
+  console.log("SESSION STORE ERROR", e)
+})
 const sessionConfig = {
+  store,
   name: "session",
-  secret: "secret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
